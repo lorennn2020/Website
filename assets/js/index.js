ee0d0ae6cc8675a9c630267bd559a18c4bc002cc
lorennn2020 <roy830817@gmail.com>
2026-06-25 16:23:20 -0700

    upload js

--- assets/js/index.js
+++ assets/js/index.js
@@ -208,12 +208,4 @@ jQuery(document).ready(function() {
         
     }, 4000);
 
-    // 點擊拷貝email
-    function copyEmail() {
-        const btn = document.getElementById('myEmail');
-        const text = btn.innerText;
-        navigator.clipboard.writeText(text)
-            .then(() => alert('Copied!'))
-            .catch(err => console.error('Copied failed', err));
-    }
 });
